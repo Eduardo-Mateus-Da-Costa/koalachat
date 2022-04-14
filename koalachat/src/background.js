5c7bf31c920421992ad5e932390481a1228da573
Eduardo Matesu Da Costa <dudu.costatx11@gmail.com>
2022-04-13 21:14:55 -0300

    ajustes no backgorund.js

--- koalachat/src/background.js
+++ koalachat/src/background.js
@@ -25,7 +25,13 @@ async function createWindow() {
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    try{
+      await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    }
+    catch(e){
+      console.log(e + "Erro no load url");
+    }
+   
     //if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
